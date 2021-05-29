9bac25544767ebdff517f3538cf71162234075f2
Filippo Viti <filippo.viti2002@gmail.com>
2021-05-29 23:42:30 +0200

    Added default option for the category filter

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,18 +1,22 @@
 function getByCategoryId(id) {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            let algorithms = JSON.parse(this.responseText);
-            let listItems = "";
-            algorithms.forEach(algorithm => listItems +=
-                `<li class="list-group-item list-group-item">
-                    <h4><a href="/algorithm/${algorithm.name}">${algorithm.name}</a></h4>
-                    ${algorithm.shortDescription}
-                </li>`
-            );
-            document.getElementById("algorithms").innerHTML = listItems;
+    if (parseInt(id) !== 0) {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let algorithms = JSON.parse(this.responseText);
+                let listItems = "";
+                algorithms.forEach(algorithm => listItems +=
+                    `<li class="list-group-item list-group-item">
+                        <h4><a href="/algorithm/${algorithm.name}">${algorithm.name}</a></h4>
+                        ${algorithm.shortDescription}
+                    </li>`
+                );
+                document.getElementById("algorithms").innerHTML = listItems;
+            }
         }
+        xmlhttp.open("GET", "/category/" + id + "/algorithms", true);
+        xmlhttp.send();
+    } else {
+        document.getElementById("algorithms").innerHTML = "";
     }
-    xmlhttp.open("GET", "/category/" + id + "/algorithms", true);
-    xmlhttp.send();
 }
